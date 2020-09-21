ef014a4fdc47e2eb537bdb9930e48155b94cf4a3
JonasRoo <jonas.roosen@outlook.com>
2020-09-21 20:45:07 +0200

    changed rebounding weight to adjust for strength of Center position | implemented new descriptive columns

--- utils/data_pipeline.py
+++ utils/data_pipeline.py
@@ -37,7 +37,8 @@ COLS_DICT = {
 LOWEST_MINS_PLAYED_TO_CONSIDER = 1
 
 # performance grading constants
-PERF_GRADING_NUMERIC_COLS = ["assists", "blocks", "rebounds", "points", "steals", "turnovers", "personal_fouls","one_pt_attempts","two_pt_attempts","three_pt_attempts"]
+PERF_GRADING_NUMERIC_COLS = ["assists", "blocks", "rebounds", "points", "steals",
+                             "turnovers", "personal_fouls", "one_pt_attempts", "two_pt_attempts", "three_pt_attempts"]
 PERF_GRADING_DEF_WEIGHT = 1
 PERF_GRADING_ATTR_WEIGHTS = {
     "points": 1,
@@ -46,8 +47,8 @@ PERF_GRADING_ATTR_WEIGHTS = {
     "assists": 1.5,
     "steals": 3,
     "blocks": 2,
-    "rebounds": 1.5,
-# new(DAVID)    
+    "rebounds": 1.25,
+    # new(DAVID)
     "one_pt_attempts": -.25,
     "two_pt_attempts": -.25,
     "three_pt_attempts": -.25,
@@ -56,9 +57,9 @@ PERF_LOWEST_POSSIBLE_SCORE = 0.001
 
 
 # TODO(jonas): docstrings
-def stats_endpoint_data_pipeline(data: List[Dict[str, Any]], min_mins_played: int = None) -> pd.DataFrame:
+def stats_endpoint_data_pipeline(data: List[Dict[str, Any]], min_mins_played: int = None, min_games_played: int = None) -> pd.DataFrame:
     if not data:
-        return pd.DataFrame()
+        raise AttributeError("data can't be `None`!")
     df = pd.DataFrame.from_dict(data)
 
     # renaming columns
@@ -73,9 +74,12 @@ def stats_endpoint_data_pipeline(data: List[Dict[str, Any]], min_mins_played: in
     # extract relevant player data, then get rid of the object
     df["player_id"] = df["player_object"].apply(lambda d: d["id"])
     df["player_position"] = df["player_object"].apply(lambda d: d["position"])
-    df["player_name"] = df["player_object"].apply(lambda d: " ".join([d["first_name"], d["last_name"]]))
+    df["player_name"] = df["player_object"].apply(
+        lambda d: " ".join([d["first_name"], d["last_name"]]))
     # game related stuff
     df["game_id"] = df["game_object"].apply(lambda d: d["id"])
+    df["is_home_game"] = df.apply(
+        lambda b: b["own_team_id"] == b["game_object"]["home_team_id"], axis=1)
 
     # now drop object columns
     # TODO(jonas): we also need to do this with the game_object (also adapt in functions)
@@ -84,17 +88,23 @@ def stats_endpoint_data_pipeline(data: List[Dict[str, Any]], min_mins_played: in
     # V-------------------- feature engineering --------------------V
     # time played
     df[["mins_played", "secs_played"]] = df["min"].str.split(":", expand=True)
-    df["mins_played"]= pd.to_numeric(df["mins_played"])
+    df["mins_played"] = pd.to_numeric(df["mins_played"])
     df["secs_played"] = pd.to_numeric(df["secs_played"])
 
     # data pre-selection
     mins_played_threshold = min_mins_played or LOWEST_MINS_PLAYED_TO_CONSIDER
     df = df.loc[df.mins_played >= mins_played_threshold]
 
+    # filter all players out that played lower than `n` games in total to avoid noise
+    if min_games_played:
+        df = remove_all_players_with_lower_than_n_games_played(
+            df=df, n_games_played=min_games_played)
+
     # performance related stuff
     df["performance_score"] = df.apply(weighted_grade_performance, axis=1)
     df["game_winning_team"] = df["game_object"].apply(get_winning_team_id)
-    df["own_team_won"] = df.apply(lambda s: True if s["game_winning_team"] == s["own_team_id"] else False, axis=1)
+    df["own_team_won"] = df.apply(
+        lambda s: True if s["game_winning_team"] == s["own_team_id"] else False, axis=1)
     df["team_points_share"] = df.apply(get_share_of_team_points, axis=1)
 
     return df
@@ -103,15 +113,17 @@ def stats_endpoint_data_pipeline(data: List[Dict[str, Any]], min_mins_played: in
 def grade_performance(row: pd.Series) -> float:
     score = 0
     for col in PERF_GRADING_NUMERIC_COLS:
-        score += row[col] * PERF_GRADING_ATTR_WEIGHTS.get(col, PERF_GRADING_DEF_WEIGHT)
-    
+        score += row[col] * \
+            PERF_GRADING_ATTR_WEIGHTS.get(col, PERF_GRADING_DEF_WEIGHT)
+
     return score / row["mins_played"]
 
 
 def weighted_grade_performance(row: pd.Series) -> float:
     unweighted_score = grade_performance(row)
     game_obj = row["game_object"]
-    total_game_points = game_obj["home_team_score"] + game_obj["visitor_team_score"]
+    total_game_points = game_obj["home_team_score"] + \
+        game_obj["visitor_team_score"]
     # TODO(jonas): should we scale this | try to minimize min-cases?
     return max(unweighted_score / total_game_points * 10e3, PERF_LOWEST_POSSIBLE_SCORE)
 
@@ -123,5 +135,13 @@ def get_winning_team_id(game: Dict[str, Any]) -> int:
 
 def get_share_of_team_points(row: pd.Series) -> float:
     game_obj = row["game_object"]
-    team = "home_team" if row["own_team_id"] == game_obj.get("home_team_id") else "visitor_team"
+    team = "home_team" if row["own_team_id"] == game_obj.get(
+        "home_team_id") else "visitor_team"
     return row["points"] / game_obj["_".join([team, "score"])]
+
+
+def remove_all_players_with_lower_than_n_games_played(df: pd.DataFrame, n_games_played: int) -> pd.DataFrame:
+    assert "player_name" in df.columns, "`player_name` needs to be in DataFrame columns!"
+    counts = df.groupby("player_name").agg({"game_id": "nunique"})
+    counts = counts.loc[counts.game_id > n_games_played]
+    return df.loc[df.player_name.isin(counts.index)]
